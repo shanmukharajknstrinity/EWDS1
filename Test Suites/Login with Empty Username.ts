720d5cdf88a2a0882d5eee80366d19cac54b4aac
shanmukha.raj <shanmukha.raj@DESKTOP-RF9NMH4.trinitymobility.com>
2024-12-12 11:44:49 +0530

    suits

---
+++ Test Suites/Login with Empty Username.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login with Empty Username</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>8e8ee38f-810d-4c44-bc48-1ea9bac490bb</testSuiteGuid>
+</TestSuiteEntity>
