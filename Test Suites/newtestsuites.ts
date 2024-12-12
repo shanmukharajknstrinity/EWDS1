14a8ba62633d41ace4de31f95d6b4f93f579152d
shanmukha.raj <shanmukha.raj@DESKTOP-RF9NMH4.trinitymobility.com>
2024-12-12 11:31:29 +0530

    new testsuites repo

---
+++ Test Suites/newtestsuites.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>newtestsuites</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>4085ad58-724b-41eb-98b0-850c02fd3cff</testSuiteGuid>
+   <testCaseLink>
+      <guid>903a5ed0-991d-4817-91fe-f7b64a97054d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/newtestcases</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
